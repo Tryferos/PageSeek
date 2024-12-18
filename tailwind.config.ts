b1a453fcb3e2b669ac7ae716374b1c7726e6eb85
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-18 18:05:23 +0200

    update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
+import type {Config} from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
@@ -14,8 +14,20 @@ const config: Config = {
         ['wotfardSb']: ['var(--font-wotfard-semibold)', 'Arial', 'sans-serif'],
       },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        secondary: '#334155',
+      },
+      scale: {
+        101: '1.01',
+        102: '1.02',
+        103: '1.03',
+        104: '1.04',
+        105: '1.05',
+      },
+      boxShadow: {
+        'book-result': '0px 0px 10px rgba(0, 0, 0, 0.1)',
+        'book-result-hover': '0px 0px 16px rgba(0, 0, 0, 0.15)',
       },
     },
   },
