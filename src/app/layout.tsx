2440a912c39ee66c77d5c5431c5a74faedce0ea6
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-19 00:31:43 +0200

    add global loader indicator

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type {Metadata} from 'next';
 import './globals.css';
 import {FontFamilies} from '@/constants/types';
+import {GlobalLoader} from '@/components/elements/GlobalLoader';
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,8 +17,9 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${FontFamilies.wotfard} antialiased font-wotfard font-wotfardRg scrollbar`}>
-        <main className="w-[100%] h-[100%] py-6 px-12 bg-gray-200">
+        <main className="w-[100%] h-[100%] py-6 px-12 bg-gray-200 relative">
           {children}
+          <GlobalLoader />
         </main>
       </body>
     </html>
---
+++ src/components/elements/GlobalLoader.tsx
@@ -0,0 +1,15 @@
+'use client';
+import {useLoading} from '@/slices/loading';
+
+export const GlobalLoader = () => {
+  const {loading} = useLoading();
+  if (!loading) {
+    return null;
+  } else {
+    return (
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center opacity-90 pointer-events-none">
+        <div className="size-16 border-[6px] border-sky-600 border-t-[transparent] rounded-[50%] animate-spin"></div>
+      </div>
+    );
+  }
+};
