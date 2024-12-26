e3711ca5c87ae6e69bf276cf513c12b5e8d4d1f9
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-26 22:41:10 +0200

    Add filtering for authors books

--- src/components/AuthorsScreen/components/AuthorBookWorks.tsx
+++ src/components/AuthorsScreen/components/AuthorBookWorks.tsx
@@ -1,22 +1,29 @@
 import {getAuthorWorksRich} from '@/network/authors';
 import {AuthorBook} from './AuthorBook';
+import {useMemo} from 'react';
 
 type Props = {
   id: string;
   name: string;
 };
 export const AuthorBookWorks = async ({id, name}: Props) => {
-  const works = await getAuthorWorksRich({key: id, limit: 7});
-  if (!works || works.entries.length < 1) {
+  const works = await getAuthorWorksRich({key: id, limit: 10});
+  const bookWorks = works?.entries.filter(
+    entry =>
+      (entry.book.description && entry.book.description.length > 0) ||
+      !!entry.book.first_publish_date ||
+      !!entry.book.ratings?.average,
+  );
+  if (!bookWorks || bookWorks.length < 1) {
     return <div>No works found</div>;
   } else {
     return (
       <div className="py-2">
         <p className="font-wotfardMd text-lg">
-          {name} is renowned for more than {works.entries.length} works
+          {name} is renowned for more than {bookWorks.length} works
         </p>
         <ul className="flex px-2 gap-x-4 overflow-x-auto py-2 scrollbar pb-4">
-          {works.entries
+          {bookWorks
             .sort(
               (a, b) =>
                 (b.book.description?.length ?? 0) -
