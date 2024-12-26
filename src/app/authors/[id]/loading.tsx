442a0ff0ffb4b97625e49fad5905d1153aa415fe
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-26 23:28:00 +0200

    add loading skeleton for author and work pages

---
+++ src/app/authors/[id]/loading.tsx
@@ -0,0 +1,5 @@
+import PageSkeleton from '@/components/elements/PageSkeleton';
+
+export default function Loading() {
+  return <PageSkeleton />;
+}
---
+++ src/components/elements/PageSkeleton.tsx
@@ -0,0 +1,34 @@
+export default function PageSkeleton() {
+  return (
+    <section className="flex w-[100%] h-full gap-y-10 flex-col">
+      <div className="flex gap-x-4 w-[100%] h-full relative">
+        <div className="h-[55vh] w-[40%] bg-gray-300 animate-pulse rounded"></div>
+        <div className="flex flex-col gap-y-2 w-[60%] h-[55vh] relative">
+          <div className="rounded-md bg-gray-300 animate-pulse h-[40px] w-[70%]"></div>
+          <div className="flex flex-col gap-y-1">
+            <div className="rounded-md bg-gray-300 animate-pulse h-[96px] w-[70%]"></div>
+          </div>
+          <div className="flex flex-col w-full gap-y-1 absolute bottom-0 left-0">
+            <div className="rounded-md bg-gray-300 animate-pulse h-[16px] w-[70%]"></div>
+            <div className="rounded-md bg-gray-300 animate-pulse h-[16px] w-[70%]"></div>
+            <div className="rounded-md bg-gray-300 animate-pulse h-[16px] w-[70%]"></div>
+            <div className="rounded-md bg-gray-300 animate-pulse h-[16px] w-[70%]"></div>
+          </div>
+        </div>
+      </div>
+      <div className="flex flex-col w-full h-[25vh] gap-y-2">
+        <div className="flex gap-x-2 w-full h-[30%]">
+          <div className="bg-gray-300 w-[20%] h-full animate-pulse rounded-xl"></div>
+          <div className="bg-gray-300 w-[20%] h-full animate-pulse rounded-xl"></div>
+          <div className="bg-gray-300 w-[20%] h-full animate-pulse rounded-xl"></div>
+          <div className="bg-gray-300 w-[20%] h-full animate-pulse rounded-xl"></div>
+        </div>
+        <div className="flex gap-y-1 h-[20%] w-full flex-col">
+          <div className="bg-gray-300 w-[calc(80%+8*4px)] h-[26px] animate-pulse rounded-xl"></div>
+          <div className="bg-gray-300 w-[calc(80%+8*4px)] h-[20px] animate-pulse rounded-xl"></div>
+          <div className="bg-gray-300 w-[calc(80%+8*4px)] h-[20px] animate-pulse rounded-xl"></div>
+        </div>
+      </div>
+    </section>
+  );
+}
