b879b8bc0733917a7d5fa790dfb62ab1dfe6d1e2
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-27 00:30:17 +0200

    adjust author books filtering

--- src/components/AuthorsScreen/components/AuthorBookWorks.tsx
+++ src/components/AuthorsScreen/components/AuthorBookWorks.tsx
@@ -13,25 +13,31 @@ export const AuthorBookWorks = async ({id, name}: Props) => {
       !!entry.book.first_publish_date ||
       !!entry.book.ratings?.average,
   );
-  if (!bookWorks || bookWorks.length < 1) {
+  console.log(works?.entries);
+  if (
+    (!bookWorks || bookWorks.length < 1) &&
+    (!works || works.entries.length < 1)
+  ) {
     return <div>No works found</div>;
   } else {
     return (
       <div className="py-2">
         <p className="font-wotfardMd text-lg">
-          {name} is renowned for more than {bookWorks.length} works
+          {name} is renowned for more than {works?.entries.length} works
         </p>
-        <ul className="flex px-2 gap-x-4 overflow-x-auto py-2 scrollbar pb-4">
-          {bookWorks
-            .sort(
-              (a, b) =>
-                (b.book.description?.length ?? 0) -
-                (a.book.description?.length ?? 0),
-            )
-            .map(entry => (
-              <AuthorBook {...entry} key={entry.key} />
-            ))}
-        </ul>
+        {
+          <ul className="flex px-2 gap-x-4 overflow-x-auto py-2 scrollbar pb-4">
+            {((bookWorks?.length === 0 ? works?.entries : bookWorks) ?? [])
+              .sort(
+                (a, b) =>
+                  (b.book.description?.length ?? 0) -
+                  (a.book.description?.length ?? 0),
+              )
+              .map(entry => (
+                <AuthorBook {...entry} key={entry.key} />
+              ))}
+          </ul>
+        }
       </div>
     );
   }
