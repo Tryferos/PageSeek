ec48bbb560eb130e7229d1ac154640a7066356e4
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-29 15:49:06 +0200

    adjust pagination reset warning popup to support both sorting and search types

--- src/components/popups_content/SortingChangeWarningContent.tsx
+++ src/components/popups_content/SortingChangeWarningContent.tsx
@@ -34,30 +34,34 @@ export const SortingChangeWarningContent = () => {
     <div className="w-full h-[23vh] py-2 flex flex-col items-center justify-between ">
       <div className="flex flex-col items-center text-center">
         <p className="font-wotfardMd text-lg text-red-600 mb-2">
-          You have changed the sorting type. This action will reset the
-          pagination!
+          You have changed the {hasChangeSortingType ? 'sorting' : 'search'}{' '}
+          method. This action will reset the pagination!
         </p>
         <p className="font-wotfardRg text-sm text-secondary">
           If you want to{' '}
           <span className="font-wotfardMd">keep the pagination</span>, click on
-          "Reset to previous Sorting".
+          "Reset to previous {hasChangeSortingType ? 'Sorting' : 'Search'}{' '}
+          method".
         </p>
         <p className="font-wotfardRg text-sm text-secondary">
           If you want to{' '}
-          <span className="font-wotfardMd">search with the new sorting</span>,
-          click on "Search with new Sorting".
+          <span className="font-wotfardMd">
+            search with the new {hasChangeSortingType ? 'sorting' : 'method'}
+          </span>
+          , click on "Search with new{' '}
+          {hasChangeSortingType ? 'Sorting' : 'method'}".
         </p>
       </div>
       <div className="flex items-end justify-center gap-x-4 w-full">
         <button
           onClick={resetSorting}
           className="px-4 py-2 outline outline-1 hover:brightness-125 outline-orange-700 hover:scale-101 transition-all rounded-md text-orange-700">
-          Reset to previous Sorting
+          Reset to previous {hasChangeSortingType ? 'Sorting' : 'Search'} method
         </button>
         <button
           onClick={goWithNewSorting}
           className="px-4 py-2 backgroundeffect rounded-md text-gray-100 hover:scale-101 ">
-          Search with new Sorting
+          Search with new {hasChangeSortingType ? 'Sorting' : 'method'}
         </button>
       </div>
     </div>
