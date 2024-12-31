2ed0bdea933a375f6090fc95d2325dce7219e841
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2024-12-31 22:12:04 +0200

    [experiment] add dvh viewport unit to fix mobile toolbar

--- src/components/HomeScreen/HomeScreen.tsx
+++ src/components/HomeScreen/HomeScreen.tsx
@@ -7,13 +7,13 @@ import {SortingTypeSelector} from './components/SortingTypeSelector';
 export default function HomeScreen() {
   return (
     <div className="flex flex-col gap-y-3 h-full w-full z-[200] bg-">
-      <div className="flex justify-between small:w-full small:items-center small:justify-evenly small:gap-x-0 gap-x-4 small:gap-x-4 z-[200] h-[50px]">
+      <div className="flex justify-between small:w-full small:items-center small:justify-evenly gap-x-4 small:gap-x-4 z-[200] h-[50px]">
         <SearchTypeSelector />
         <SearchMode />
         <MobileFilters />
         <SortingSelector />
       </div>
-      <div className="h-[calc(100vh-50px-12px-24px-70px-3.5vh)] z-[100] small:h-[calc(100vh-50px-12px-16px-70px-3.5vh)]">
+      <div className="h-[calc(100vh-50px-12px-24px-70px-3.5vh)] z-[100] small:h-[calc(100dvh-50px-12px-16px-70px-3.5vh)]">
         <SearchResults />
       </div>
     </div>
