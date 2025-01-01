a2f50d1581a15df58cad092b3ee6df889912309e
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-01 15:03:30 +0200

    adjust book texts

--- src/components/HomeScreen/components/Welcome.tsx
+++ src/components/HomeScreen/components/Welcome.tsx
@@ -31,22 +31,22 @@ const Books = () => {
     <div className="absolute right-24 bottom-12 mobile:hidden z-[60] small:right-12 small:scale-[0.8]">
       <div className="h-[30px] w-[120px] bg-orange-600 rotate-[115deg] shadow-[2px_-3px_0px_#000] booknoise relative flex justify-end">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
-          Fiction
+          Science
         </p>
       </div>
       <div className="h-[30px] absolute left-10 bottom-3 w-[150px] bg-cyan-700 rotate-[90deg] shadow-[2px_-3px_0px_#000] booknoise flex justify-end">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
-          Science
+          Fiction
         </p>
       </div>
       <div className="h-[30px] absolute -left-[74.9vw] bottom-[calc(-50%-12px)] w-[110px] bg-yellow-600 rotate-[155deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
-          Music
+          Cooking
         </p>
       </div>
       <div className="h-[30px] absolute -left-[calc(76vw-100px)] -bottom-12 w-[100px] bg-lime-700 rotate-[180deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
-          Cooking
+          Music
         </p>
       </div>
     </div>
