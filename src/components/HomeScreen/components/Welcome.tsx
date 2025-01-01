b3da878d82454fb41712967836331d631d56def7
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-01 12:19:31 +0200

    add books to fill in blank space

--- src/components/HomeScreen/components/Welcome.tsx
+++ src/components/HomeScreen/components/Welcome.tsx
@@ -21,6 +21,34 @@ export const Welcome = () => {
           fill
         />
       </figure>
+      <Books />
+    </div>
+  );
+};
+
+const Books = () => {
+  return (
+    <div className="absolute right-10 bottom-12 mobile:hidden">
+      <div className="h-[30px] w-[120px] bg-orange-600 rotate-[115deg] shadow-[2px_-3px_0px_#000] booknoise relative flex justify-end">
+        <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
+          Fiction
+        </p>
+      </div>
+      <div className="h-[30px] absolute left-10 bottom-3 w-[150px] bg-cyan-700 rotate-[90deg] shadow-[2px_-3px_0px_#000] booknoise flex justify-end">
+        <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
+          Science
+        </p>
+      </div>
+      <div className="h-[30px] absolute -left-[78.9vw] bottom-[calc(-50%-12px)] w-[110px] bg-yellow-600 rotate-[155deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
+        <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
+          Music
+        </p>
+      </div>
+      <div className="h-[30px] absolute -left-[calc(80vw-100px)] -bottom-12 w-[100px] bg-lime-700 rotate-[180deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
+        <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
+          Cooking
+        </p>
+      </div>
     </div>
   );
 };
