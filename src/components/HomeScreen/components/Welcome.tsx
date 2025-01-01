e1fdd4f61bdf0cef1fef094a300b49d7c15ddb1f
Trifon Mazarakis <101869298+Tryferos@users.noreply.github.com>
2025-01-01 15:01:03 +0200

    adjust welcome component

--- src/components/HomeScreen/components/Welcome.tsx
+++ src/components/HomeScreen/components/Welcome.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image';
 
 export const Welcome = () => {
   return (
-    <div className="items-center flex flex-col justify-between h-[64.4vh] pt-4 mt-10 relative">
+    <div className="items-center flex flex-col justify-between h-[64.4vh] pt-4 small:mt-10 mt-16 relative">
       <div className="flex flex-col gap-y-2 items-center">
         <p className="texteffect text-2xl font-wotfardSb">PageSeek</p>
         <p className="font-wotfardRg text-xl text-center">
@@ -13,7 +13,7 @@ export const Welcome = () => {
           </span>
         </p>
       </div>
-      <figure className="relative w-full h-[25vw] min-h-[250px] mt-20">
+      <figure className="relative w-full h-[25vw] min-h-[250px] mt-20 z-[60] bg-transparent">
         <Image
           priority
           src={'/books-welcome.svg'}
@@ -28,7 +28,7 @@ export const Welcome = () => {
 
 const Books = () => {
   return (
-    <div className="absolute right-10 bottom-12 mobile:hidden">
+    <div className="absolute right-24 bottom-12 mobile:hidden z-[60] small:right-12 small:scale-[0.8]">
       <div className="h-[30px] w-[120px] bg-orange-600 rotate-[115deg] shadow-[2px_-3px_0px_#000] booknoise relative flex justify-end">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
           Fiction
@@ -39,12 +39,12 @@ const Books = () => {
           Science
         </p>
       </div>
-      <div className="h-[30px] absolute -left-[78.9vw] bottom-[calc(-50%-12px)] w-[110px] bg-yellow-600 rotate-[155deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
+      <div className="h-[30px] absolute -left-[74.9vw] bottom-[calc(-50%-12px)] w-[110px] bg-yellow-600 rotate-[155deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
           Music
         </p>
       </div>
-      <div className="h-[30px] absolute -left-[calc(80vw-100px)] -bottom-12 w-[100px] bg-lime-700 rotate-[180deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
+      <div className="h-[30px] absolute -left-[calc(76vw-100px)] -bottom-12 w-[100px] bg-lime-700 rotate-[180deg] shadow-[-2px_-3px_0px_#000] booknoise flex justify-end small:hidden">
         <p className="text-md rotate-180 text-gray-100 font-wotfardMd absolute bottom-[2px] right-2">
           Cooking
         </p>
